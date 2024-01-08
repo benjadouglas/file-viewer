eba7b4f3f1f7bdbb1622deffd9005c8648a6823f
DOUGLAS REZZONICO BENJAMÍN <82091184+benjadouglas@users.noreply.github.com>
2024-01-07 21:54:45 -0300

    added changes from the main.rs file in classic-ui branch

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,6 @@
-use axum::{extract::Query, response::Json, routing::get, Router};
+use axum::{extract::Query, response::Json, routing::get, Router, http::{StatusCode,Response}};
 use serde::{Deserialize, Serialize};
-#[allow(unused)]
-use std::{env, net::SocketAddr, path::Path};
+use std::{env, net::SocketAddr, path::Path, process::Command};
 use tower_http::cors::CorsLayer;
 // use serde_json::to_string;
 
@@ -24,9 +23,9 @@ pub struct my_dir {
 
 #[tokio::main]
 async fn main() {
-    // filesV2("/benja/Code/web/file_viewer/backend/src");//32768
     let app = Router::new()
         .route("/", get(index))
+        .route("/open/terminal", get(open_terminal))
         //allow frontend to fetch data
         .layer(CorsLayer::very_permissive());
     let adress = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -34,11 +33,12 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
-}
+} 
 
 async fn index(Query(params): Query<req_handler>) -> Json<my_dir> {
     // let arr = serial_killer("/benja/Code/web/file_viewer/frontend/");
-    let temp = params.file.as_deref().unwrap_or("/");
+    let temp = params.file.as_deref().unwrap_or("");
+    println!("In fn index temp= {}", temp);
     let arr = serial_killer(&format!("{}", temp));
     Json(arr)
 }
@@ -59,26 +59,10 @@ fn serial_killer(s: &str) -> my_dir {
     return obj;
 }
 
-fn get_dirs(s: &str) -> Vec<String> {
-    let home_dir = env::var("HOME").expect("HOME environment variable not set");
-    let dir_path = format!("{}{}", home_dir, s);
-    let dir = Path::new(&dir_path);
-    let mut arr: Vec<String> = Vec::new();
-    for entry in dir.read_dir().expect("Couldn't read dir") {
-        match entry {
-            Ok(entry) => {
-                let path = entry.path();
-                arr.push(path.to_str().unwrap().to_string());
-            }
-            Err(e) => println!("Error reading directory: {}", e),
-        }
-    }
-    return arr;
-}
 
 fn files(s: &str) -> Vec<String> {
     let home_dir = env::var("HOME").expect("HOME environment variable not set");
-    let dir_path = format!("{}{}", home_dir, s);
+    let dir_path = format!("{}/{}", home_dir, s);
     let dir = Path::new(&dir_path);
     let mut arr: Vec<String> = Vec::new();
     for entry in dir.read_dir().expect("Couldn't read dir") {
@@ -94,9 +78,9 @@ fn files(s: &str) -> Vec<String> {
     return arr;
 }
 
-fn filesV2(s: &str) -> Vec<(String,String)> {
+fn filesV2(s: &str) -> Vec<(String, String)> {
     let home_dir = env::var("HOME").expect("HOME environment variable not set");
-    let dir_path = format!("{}{}", home_dir, s);
+    let dir_path = format!("{}/{}", home_dir, s);
     let dir = Path::new(&dir_path);
     let mut arr: Vec<(String, String)> = Vec::new();
     for entry in dir.read_dir().expect("Couldn't read dir") {
@@ -126,5 +110,26 @@ fn filesV2(s: &str) -> Vec<(String,String)> {
             Err(e) => println!("Error reading directory: {}", e),
         }
     }
-    return arr
+    return arr;
+}
+
+async fn open_terminal(Query(params): Query<req_handler>) -> &'static str{
+    let s = params.file.as_deref().unwrap_or("/");
+    let script = format!(
+        "tell application \"iTerm\"
+            set newWindow to (create window with default profile)
+            tell newWindow
+                tell the current session
+                    write text \"cd ~{} && vim .\"
+                end tell
+            end tell
+        end tell",
+        s
+    );
+    Command::new("osascript")
+        .arg("-e")
+        .arg(script)
+        .spawn()
+        .expect("Failed to open terminal");
+    return "ok";
 }
